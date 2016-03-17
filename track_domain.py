77cd6fe2f9d81ffb2202925036850545a87b0044
T Hennen <tyler@hennen.us>
2016-03-17 09:09:33 -0700

    Save and reuse ROI.  Save new figures.  watermark.

--- track_domain.py
+++ track_domain.py
@@ -19,9 +19,10 @@ from skimage import exposure
 from fnmatch import filter
 from os.path import join as pjoin
 from os.path import isdir
+from shutil import copyfile
 
 
-def track_domain(imdir):
+def track_domain(imdir, repeat_ROI=False):
     # Make new directory to store result of analysis
     contour_dir = pjoin(imdir, 'contours')
     if not isdir(contour_dir):
@@ -47,23 +48,38 @@ def track_domain(imdir):
 
     # Plot 10 of the images on top of eachother for area selection
     print('Plotting from {}'.format(imdir))
-    fig, ax = plt.subplots()
+    fig, ax = make_fig(np.shape(ims[0]))
     step = max(1, len(ims) / 10)
     for im in ims[::step]:
         ax.imshow(im, alpha=.1, cmap='gray')
     ax.imshow(ims[-1], alpha=.1, cmap='gray')
 
-    a = SelectRect(ax)
-    plt.show()
-    while a.x is None:
-        plt.pause(.1)
+    if repeat_ROI:
+        roi = pjoin(contour_dir, 'ROI.txt')
+        if os.path.isfile(roi):
+            with open(roi, 'r') as f:
+                roi_str = f.readline()
+                [(i0, i1), (j0, j1)] = [s.split(':') for s in roi_str.split(',')]
+                i0, i1, j0, j1 = int(i0), int(i1), int(j0), int(j1)
+        else:
+            print('ROI.txt not found')
+            repeat_ROI = False
 
-    # Change to matrix notation
-    i0, i1 = a.y[0], a.y[1]
-    j0, j1 = a.x[0], a.x[1]
+    if not repeat_ROI:
+        a = SelectRect(ax)
+        plt.show()
+        while a.x is None:
+            plt.pause(.1)
+
+        # Change to matrix notation
+        i0, i1 = a.y[0], a.y[1]
+        j0, j1 = a.x[0], a.x[1]
+
     di = i1 - i0
     dj = j1 - j0
     subims = [im[i0:i1, j0:j1] for im in ims]
+
+    fig.savefig(pjoin(contour_dir, 'overlap.png'), pad_inches='tight')
 
     x1 = []
     x2 = []
@@ -71,6 +87,7 @@ def track_domain(imdir):
     y2 = []
 
     plt.ioff()
+    bubbles = []
     for subim, fn in zip(subims, imfns):
         fig, ax = make_fig((di, dj))
         ax.imshow(subim, cmap='gray', interpolation='none')
@@ -78,7 +95,9 @@ def track_domain(imdir):
         level = (np.max(filt_subim) + np.min(filt_subim)) / 2
         contours = find_contours(filt_subim, level)
 
+        # pick the longest contour.  not always right.
         bubble = max(contours, key=len)
+        bubbles.append(bubble)
         bubblex = bubble[:, 1]
         bubbley = bubble[:, 0]
 
@@ -91,11 +110,34 @@ def track_domain(imdir):
         ax.plot(bubblex, bubbley, linewidth=2, c='Lime')
         ax.hlines((y1[-1], y2[-1]), 0, dj, linestyles='dashed')
         ax.vlines((x1[-1], x2[-1]), 0, di, linestyles='dashed')
-        fig.savefig(pjoin(contour_dir, fn))
+        fig.savefig(pjoin(contour_dir, fn), pad_inches='tight')
         plt.close(fig)
 
     with open(pjoin(contour_dir, 'ROI.txt'), 'w') as f:
         f.write('{}:{}, {}:{}'.format(i0, i1, j0, j1))
+
+    # Plot all the contours
+    fig, ax = make_fig((di, dj))
+    ax.invert_yaxis()
+    cmap = plt.cm.hsv
+    colors = [cmap(q) for q in np.linspace(0, 1, len(bubbles))]
+    for bub, c in zip(bubbles, colors):
+        ax.plot(bub[:, 1], bub[:, 0], linewidth=1.5, c=c)
+    fig.savefig(pjoin(contour_dir, 'all_contours.png'), pad_inches='tight')
+
+    # Add last image
+    ax.imshow(subims[-1], cmap='gray')
+    fontdict = {'size':8}
+    mpvalue = np.mean(subims[-1][-15:, -50:])
+    if mpvalue > .5:
+        fontdict['color'] = 'black'
+    else:
+        fontdict['color'] = 'white'
+    ax.text(dj - 55, di - 3, 'T Hennen', fontdict=fontdict)
+    fig.savefig(pjoin(contour_dir, 'all_contours2.png'), pad_inches='tight')
+
+    plt.close(fig)
+
     plt.ion()
     return (imnums, x1, x2, y1, y2)
 
@@ -127,7 +169,7 @@ def write_plots((imnums, x1, x2, y1, y2), plotdir):
     ax1.set_xlabel('File Number')
     ax1.set_ylabel('Location (pixels)')
     plt.legend(loc=0)
-    fig1.savefig(pjoin(plotdir, 'left_right.png'))
+    fig1.savefig(pjoin(plotdir, 'left_right.png'), pad_inches='tight')
     plt.close(fig1)
 
     fig2, ax2 = plt.subplots()
@@ -136,7 +178,7 @@ def write_plots((imnums, x1, x2, y1, y2), plotdir):
     ax2.set_xlabel('File Number')
     ax2.set_ylabel('Location (pixels)')
     plt.legend(loc=0)
-    fig2.savefig(pjoin(plotdir, 'top_bottom.png'))
+    fig2.savefig(pjoin(plotdir, 'top_bottom.png'), pad_inches='tight')
     plt.close(fig2)
 
     fig3, ax3 = plt.subplots()
@@ -145,7 +187,7 @@ def write_plots((imnums, x1, x2, y1, y2), plotdir):
     ax3.set_xlabel('File Number')
     ax3.set_ylabel('$\\Delta$ Location (pixels)')
     plt.legend(loc=0)
-    fig3.savefig(pjoin(plotdir, 'diff_top_bottom.png'))
+    fig3.savefig(pjoin(plotdir, 'diff_top_bottom.png'), pad_inches='tight')
     plt.close(fig3)
 
     fig4, ax4 = plt.subplots()
@@ -154,7 +196,7 @@ def write_plots((imnums, x1, x2, y1, y2), plotdir):
     ax4.set_xlabel('File Number')
     ax4.set_ylabel('$\\Delta$ Location (pixels)')
     plt.legend(loc=0)
-    fig4.savefig(pjoin(plotdir, 'diff_left_right.png'))
+    fig4.savefig(pjoin(plotdir, 'diff_left_right.png'), pad_inches='tight')
     plt.close(fig4)
 
     plt.ion()
@@ -169,23 +211,30 @@ def write_data((imnums, x1, x2, y1, y2), datadir):
         np.savetxt(f, zip(imnums, x1, x2, y1, y2), delimiter='\t', fmt=fmt)
 
 
-def analyze_all(dir=r'\\132.239.170.55\SharableDMIsamples\H31', skip=0):
+def analyze_all(dir=r'\\132.239.170.55\SharableDMIsamples\H31', skip=0, **kwargs):
     '''
     Analyze all subdirs, writing plots and extracted data to that subdir.
     Also write summary to parent dir
     '''
-    with open(pjoin(dir, 'contour_edges.txt'), 'w') as summary_file:
+    analysis_dir = pjoin(dir, 'Analysis')
+    if not os.path.isdir(analysis_dir):
+        os.makedirs(analysis_dir)
+    with open(pjoin(analysis_dir, 'contour_edges.txt'), 'w') as summary_file:
         summary_file.write('image_num\tleft\tright\ttop\tbottom\n')
         fmt = ['%d', '%.2f', '%.2f', '%.2f', '%.2f']
         folders = [f for f in os.listdir(dir)[skip:] if isdir(pjoin(dir, f))]
         for folder in folders:
-            path = pjoin(dir, folder)
-            dout = track_domain(path)
-            write_plots(dout, path)
-            write_data(dout, path)
-            summary_file.write('#{}\n'.format(folder))
-            np.savetxt(summary_file, zip(*dout), delimiter='\t', fmt=fmt)
-            plt.close()
+            if folder != 'Analysis':
+                path = pjoin(dir, folder)
+                dout = track_domain(path, **kwargs)
+                write_plots(dout, path)
+                write_data(dout, path)
+                # When done, collect some images and data into analysis directory
+                summary_file.write('#{}\n'.format(folder))
+                np.savetxt(summary_file, zip(*dout), delimiter='\t', fmt=fmt)
+                allcontours = pjoin(dir, folder, 'contours', 'all_contours2.png')
+                copyfile(allcontours, pjoin(analysis_dir, folder+'_contours.png'))
+                plt.close()
 
 
 def make_fig(shape, dpi=96.):
@@ -200,6 +249,7 @@ def make_fig(shape, dpi=96.):
     return fig, ax
 
 if __name__ == '__main__':
-    imdir = r"Tolley DMI PtCoIrPt\Ir 2A\1"
+    pass
+    #imdir = r"Tolley DMI PtCoIrPt\Ir 2A\1"
     #imdir= r'C:\Users\thenn\Desktop\bubbles\Tolley DMI PtCoIrPt\Ir 4A\15'
-    track_domain(imdir)
+    #track_domain(imdir)
